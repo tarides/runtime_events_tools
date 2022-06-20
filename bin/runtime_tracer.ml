7b757eba371ac233461a292b0a3d01640e0a53ad
Sadiq Jaffer <sadiq@toao.com>
2022-06-20 15:37:47 +0100

    clear up runtime_tracer

--- bin/runtime_tracer.ml
+++ bin/runtime_tracer.ml
@@ -1,14 +1,16 @@
 let () =
   if Array.length Sys.argv < 3 then
-    Printf.printf "trace_prog <trace_filename> <executable> <args> ...\n"
+    Printf.printf "runtime_tracer <trace_filename> <executable> <args> ...\n"
   else begin
     (* Parse arguments and set up environment *)
     let trace_filename = Array.get Sys.argv 1 in
     let trace_file = open_out trace_filename in
     Printf.fprintf trace_file "[";
     let executable_filename = Array.get Sys.argv 2 in
-    let args = if Array.length Sys.argv > 3 then Array.sub Sys.argv 3 (Array.length Sys.argv) else [||] in
-    let env = [Unix.environment (); [| "OCAML_RUNTIME_EVENTS_START=1"; "OCAML_RUNTIME_EVENTS_DIR=/tmp/" |]] |> Array.concat in
+    let args = if Array.length Sys.argv > 3 then Array.sub Sys.argv 2 (Array.length Sys.argv - 2) else [||] in
+    (* Set the temp directory. We should make this configurable. *)
+    let tmp_dir = "/tmp/" in
+    let env = [Unix.environment (); [| "OCAML_RUNTIME_EVENTS_START=1"; "OCAML_RUNTIME_EVENTS_DIR=" ^ tmp_dir; "OCAML_RUNTIME_EVENTS_PRESERVE=1" |]] |> Array.concat in
     let child_pid = Unix.create_process_env executable_filename args env Unix.stdin Unix.stdout Unix.stderr in
     (* Helper for callbacks *)
     let ts_to_ms ts = Int64.(div (Runtime_events.Timestamp.to_int64 ts) (of_int 1000)) in
@@ -23,7 +25,7 @@ let () =
       flush trace_file in
     (* Read from the child process *)
     Unix.sleep 1;
-    let cursor = Runtime_events.create_cursor (Some ("/tmp", child_pid)) in
+    let cursor = Runtime_events.create_cursor (Some (tmp_dir, child_pid)) in
     let callbacks = Runtime_events.Callbacks.create ~runtime_begin ~runtime_end () in
     let child_alive () = match Unix.waitpid [ Unix.WNOHANG ] child_pid with
       | (0, _) -> true
@@ -32,5 +34,9 @@ let () =
     while child_alive () do begin
       Runtime_events.read_poll cursor callbacks None |> ignore
     end done;
-    Printf.fprintf trace_file "]\n";
+    (* Now we're done, we need to remove the ring buffers ourselves because we
+      told the child process not to remove them *)
+    let ring_file =
+        Filename.concat tmp_dir (string_of_int child_pid ^ ".events") in
+    Unix.unlink ring_file
   end
