aa85aef9179fdfa6811847309244197597c12aba
Tim McGilchrist <timmcgil@gmail.com>
2024-02-14 12:31:44 +1100

    Convert comment to TODO

--- bin/olly.ml
+++ bin/olly.ml
@@ -101,7 +101,7 @@ let olly ?extra ~runtime_begin ~runtime_end ~cleanup ~lifecycle ~init exec_args
   let argsl = String.split_on_char ' ' exec_args in
   let executable_filename = List.hd argsl in
 
-  (* Set the temp directory. We should make this configurable. *)
+  (* TODO Set the temp directory. We should make this configurable. *)
   let tmp_dir = Filename.get_temp_dir_name () |> Unix.realpath in
   let env =
     Array.append
