ee63adb4da79ad39d13ad7c3b9e907454390949a
Tim McGilchrist <timmcgil@gmail.com>
2026-03-26 16:14:46 +1100

    Reinstate olly latency command.
    This is referenced from the OCaml manual versions 5.0 to 5.4

--- lib/olly_gc_stats/olly_gc_stats.ml
+++ lib/olly_gc_stats/olly_gc_stats.ml
@@ -107,6 +107,108 @@ let print_percentiles json output hist =
         Printf.fprintf oc "%.4f \t %.2f\n" p
           (float_of_int (H.value_at_percentile hist p) |> ms)))
 
+let print_latency_only json output hist =
+  let ms ns = ns /. 1_000_000. in
+
+  let mean_latency = H.mean hist |> ms
+  and max_latency = float_of_int (H.max hist) |> ms in
+  let percentiles =
+    [|
+      25.0;
+      50.0;
+      60.0;
+      70.0;
+      75.0;
+      80.0;
+      85.0;
+      90.0;
+      95.0;
+      96.0;
+      97.0;
+      98.0;
+      99.0;
+      99.9;
+      99.99;
+      99.999;
+      99.9999;
+      100.0;
+    |]
+  in
+  let oc = match output with Some s -> open_out s | None -> stderr in
+
+  if json then
+    let distribs =
+      List.init (Array.length percentiles) (fun i ->
+          let percentile = percentiles.(i) in
+          let value =
+            H.value_at_percentile hist percentiles.(i)
+            |> float_of_int |> ms |> string_of_float
+          in
+          Printf.sprintf "\"%.4f\": %s" percentile value)
+      |> String.concat ","
+    in
+    Printf.fprintf oc
+      {|{"mean_latency": %f, "max_latency": %f, "distr_latency": {%s}}|}
+      mean_latency max_latency distribs
+  else (
+    Printf.fprintf oc "\n";
+    Printf.fprintf oc "GC latency profile:\n";
+    Printf.fprintf oc "#[Mean (ms):\t%.2f,\t Stddev (ms):\t%.2f]\n" mean_latency
+      (H.stddev hist |> ms);
+    Printf.fprintf oc "#[Min (ms):\t%.2f,\t max (ms):\t%.2f]\n"
+      (float_of_int (H.min hist) |> ms)
+      max_latency;
+    Printf.fprintf oc "\n";
+    Printf.fprintf oc "Percentile \t Latency (ms)\n";
+    Fun.flip Array.iter percentiles (fun p ->
+        Printf.fprintf oc "%.4f \t %.2f\n" p
+          (float_of_int (H.value_at_percentile hist p) |> ms)))
+
+let latency poll_sleep json output runtime_events_dir exec_args =
+  let current_event = Hashtbl.create 13 in
+  let hist =
+    H.init ~lowest_discernible_value:10 ~highest_trackable_value:10_000_000_000
+      ~significant_figures:3
+  in
+  let is_gc_phase phase =
+    match phase with
+    | Runtime_events.EV_MAJOR | Runtime_events.EV_STW_LEADER
+    | Runtime_events.EV_INTERRUPT_REMOTE ->
+        true
+    | _ -> false
+  in
+  let runtime_begin ring_id ts phase =
+    if is_gc_phase phase then
+      match Hashtbl.find_opt current_event ring_id with
+      | None -> Hashtbl.add current_event ring_id (phase, Ts.to_int64 ts)
+      | _ -> ()
+  in
+  let runtime_end ring_id ts phase =
+    match Hashtbl.find_opt current_event ring_id with
+    | Some (saved_phase, saved_ts) when saved_phase = phase ->
+        Hashtbl.remove current_event ring_id;
+        let latency = Int64.to_int (Int64.sub (Ts.to_int64 ts) saved_ts) in
+        assert (H.record_value hist latency)
+    | _ -> ()
+  in
+  let init = Fun.id in
+  let cleanup () = print_latency_only json output hist in
+  let open Olly_common.Launch in
+  try
+    `Ok
+      (olly
+         {
+           empty_config with
+           runtime_begin;
+           runtime_end;
+           init;
+           cleanup;
+           poll_sleep;
+           runtime_events_dir;
+         }
+         exec_args)
+  with Fail msg -> `Error (false, msg)
+
 let gc_stats poll_sleep json output runtime_events_dir exec_args =
   let current_event = Hashtbl.create 13 in
   let hist =
@@ -206,4 +308,41 @@ let gc_stats_cmd =
     Term.(
       ret
         (const gc_stats $ freq_option $ json_option $ output_option
+       $ runtime_events_dir $ exec_args 0))
+
+let latency_cmd =
+  let open Cmdliner in
+  let open Olly_common.Cli in
+  let json_option =
+    let doc = "Print the output in json instead of human-readable format." in
+    Arg.(value & flag & info [ "json" ] ~docv:"json" ~doc)
+  in
+
+  let output_option =
+    let doc =
+      "Redirect the output of `olly` to specified file. The output of the \
+       command is not redirected."
+    in
+    Arg.(
+      value
+      & opt (some string) None
+      & info [ "o"; "output" ] ~docv:"output" ~doc)
+  in
+
+  let man =
+    [
+      `S Manpage.s_description;
+      `P
+        "Report the GC latency profile. This includes mean, standard deviation \
+         and percentile latency profile of GC events.";
+      `Blocks help_secs;
+    ]
+  in
+  let doc = "Report the GC latency profile." in
+  let info = Cmd.info "latency" ~doc ~sdocs ~man in
+
+  Cmd.v info
+    Term.(
+      ret
+        (const latency $ freq_option $ json_option $ output_option
        $ runtime_events_dir $ exec_args 0))
