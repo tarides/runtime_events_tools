e7fd279d715e3fa17a1db602dac7b5b3375ec9e3
Tim McGilchrist <timmcgil@gmail.com>
2026-03-30 17:04:24 +1100

    Make tests cross-platform

--- test/test_launch.ml
+++ test/test_launch.ml
@@ -8,8 +8,12 @@ let process_launch_failure () =
     (fun () -> ignore (Launch.exec_process config [ "missing.exe" ]));
 
   match_raises "non-executable should not launch"
-    (* File for exec_process is not an executable *)
-    (function Unix.Unix_error (Unix.EACCES, _, _) -> true | _exn -> false)
+    (* File for exec_process is not an executable.
+       Unix returns EACCES, Windows returns ENOEXEC. *)
+    (function
+      | Unix.Unix_error (Unix.EACCES, _, _) -> true
+      | Unix.Unix_error (Unix.ENOEXEC, _, _) -> true
+      | _exn -> false)
     (fun () -> ignore (Launch.exec_process config [ "./run_endlessly.ml" ]));
 
   match_raises "empty executable string should not launch"
@@ -24,11 +28,17 @@ let process_launch () =
     "process should launch" true
     (try
        let a = Launch.exec_process config [ "./run_endlessly.exe" ] in
-       try
-         (* Sending signal Zero to kill checks the process exists for Unix. *)
-         Unix.kill a.pid 0;
-         true
-       with Unix.Unix_error (Unix.ESRCH, _, _) -> false
+       (* Check the child is alive using waitpid WNOHANG.
+          Unix.kill with signal 0 is not supported on Windows. *)
+       let launched = a.alive () in
+       (* Clean up the child process so it doesn't leak.
+          On Windows, open files cannot be deleted, so the orphan process
+          would prevent dune from cleaning up its temp directory. *)
+       (try Unix.kill a.pid Sys.sigkill
+        with Unix.Unix_error _ | Invalid_argument _ -> ());
+       (try ignore (Unix.waitpid [] a.pid) with Unix.Unix_error _ -> ());
+       a.close ();
+       launched
      with
     (* Any exceptions indicate a failure to launch *)
     | Unix.Unix_error (Unix.ENOENT, _, _) -> false
