a799c75daa770284b9dabe4bc4de5e8066d44cd6
Tim McGilchrist <timmcgil@gmail.com>
2026-04-14 12:17:23 +1000

    Replace Printf.fprintf with a reusable Buffer.t
    This should avoid allocations in Printf (1-2 allocations), Some(fun oc
    -> ...) closure + option wrapper for counters, and %t function
    argument indirection.

--- lib/olly_trace/olly_format_backend/olly_format_json.ml
+++ lib/olly_trace/olly_format_backend/olly_format_json.ml
@@ -3,32 +3,54 @@ open Olly_format_backend
 let name = "json"
 let description = "Chrome Trace Format"
 
-type trace = { file : out_channel }
+type trace = { file : out_channel; buf : Buffer.t }
 
 let create ~filename =
   let file = open_out filename in
-  Printf.fprintf file "[";
-  { file }
+  output_string file "[";
+  { file; buf = Buffer.create 256 }
 
 let close trace = close_out trace.file
-let ts_to_us ts = Int64.(div ts (of_int 1000))
 
-let write_json trace ~name ~ts ~ring_id ph args =
-  Printf.fprintf trace.file
-    "{\"name\": \"%s\", \"cat\": \"PERF\", \"ph\":\"%s\", \"ts\":%Ld, \"pid\": \
-     %d, \"tid\": %d%t},\n"
-    name ph (ts_to_us ts) ring_id ring_id
-    (match args with
-    | None -> ignore
-    | Some args -> fun oc -> Printf.fprintf oc ", \"args\": %t" args)
+let write_span trace ~name ~ts ~ring_id ph =
+  let buf = trace.buf in
+  Buffer.clear buf;
+  Buffer.add_string buf "{\"name\": \"";
+  Buffer.add_string buf name;
+  Buffer.add_string buf "\", \"cat\": \"PERF\", \"ph\":\"";
+  Buffer.add_string buf ph;
+  Buffer.add_string buf "\", \"ts\":";
+  Buffer.add_string buf (Int64.to_string (Int64.div ts 1000L));
+  Buffer.add_string buf ", \"pid\": ";
+  Buffer.add_string buf (string_of_int ring_id);
+  Buffer.add_string buf ", \"tid\": ";
+  Buffer.add_string buf (string_of_int ring_id);
+  Buffer.add_string buf "},\n";
+  Buffer.output_buffer trace.file buf
+
+let write_counter trace ~name ~ts ~ring_id value =
+  let buf = trace.buf in
+  Buffer.clear buf;
+  Buffer.add_string buf "{\"name\": \"";
+  Buffer.add_string buf name;
+  Buffer.add_string buf "\", \"cat\": \"PERF\", \"ph\":\"C\", \"ts\":";
+  Buffer.add_string buf (Int64.to_string (Int64.div ts 1000L));
+  Buffer.add_string buf ", \"pid\": ";
+  Buffer.add_string buf (string_of_int ring_id);
+  Buffer.add_string buf ", \"tid\": ";
+  Buffer.add_string buf (string_of_int ring_id);
+  Buffer.add_string buf ", \"args\": {\"";
+  Buffer.add_string buf name;
+  Buffer.add_string buf "\": ";
+  Buffer.add_string buf (string_of_int value);
+  Buffer.add_string buf "}},\n";
+  Buffer.output_buffer trace.file buf
 
 let emit trace ~ring_id ~ts ~name ~kind =
   let open Event in
   match kind with
-  | SpanBegin -> write_json trace ~name ~ts ~ring_id "B" None
-  | SpanEnd -> write_json trace ~name ~ts ~ring_id "E" None
-  | Counter value ->
-      Some (fun oc -> Printf.fprintf oc "{\"%s\": %d}" name value)
-      |> write_json trace ~name ~ts ~ring_id "C"
-  | Instant -> write_json trace ~name ~ts ~ring_id "i" None
+  | SpanBegin -> write_span trace ~name ~ts ~ring_id "B"
+  | SpanEnd -> write_span trace ~name ~ts ~ring_id "E"
+  | Counter value -> write_counter trace ~name ~ts ~ring_id value
+  | Instant -> write_span trace ~name ~ts ~ring_id "i"
   | _ -> ()
