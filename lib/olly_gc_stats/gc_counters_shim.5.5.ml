8c06c674bac51aadba991eb913cc7b734a1da62e
Tim McGilchrist <timmcgil@gmail.com>
2026-04-17 17:11:00 +1000

    fixup! Add OCaml 5.5 implementation of gc_stats using word sized counters

--- lib/olly_gc_stats/gc_counters_shim.5.5.ml
+++ lib/olly_gc_stats/gc_counters_shim.5.5.ml
@@ -6,8 +6,7 @@ let runtime_counter ~domain_minor_words ~domain_promoted_words
     ~domain_major_words ring_id _ts counter_type value =
   match counter_type with
   | Runtime_events.EV_C_MINOR_PROMOTED_WORDS ->
-      domain_promoted_words.(ring_id) <-
-        domain_promoted_words.(ring_id) + value
+      domain_promoted_words.(ring_id) <- domain_promoted_words.(ring_id) + value
   | Runtime_events.EV_C_MINOR_ALLOCATED_WORDS ->
       domain_minor_words.(ring_id) <- domain_minor_words.(ring_id) + value
   | Runtime_events.EV_C_MAJOR_ALLOCATED_WORDS ->
