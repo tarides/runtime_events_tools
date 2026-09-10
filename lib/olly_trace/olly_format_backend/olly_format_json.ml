6acfd4522b1d78b3f0c64dde9a89831ab6fcc46f
Edwin Török <edwin@tarides.com>
2026-09-10 10:35:44 +0100

    olly_trace: emit correct JSON arrays

--- lib/olly_trace/olly_format_backend/olly_format_json.ml
+++ lib/olly_trace/olly_format_backend/olly_format_json.ml
@@ -3,21 +3,28 @@ open Olly_format_backend
 let name = "json"
 let description = "Chrome Trace Format"
 
-type trace = { file : out_channel }
+type trace = { file : out_channel; mutable first : bool }
 
 let create ~filename =
   let file = open_out filename in
   Printf.fprintf file "[";
-  { file }
+  { file; first = true }
 
-let close trace = close_out trace.file
+let[@inline] array_elt_begin trace =
+  if trace.first then trace.first <- false else Printf.fprintf trace.file ","
+
+let close trace =
+  Printf.fprintf trace.file "]";
+  close_out trace.file
+
 let ts_to_us ts = Int64.(div ts (of_int 1000))
 
 let write_json trace evt ph args =
   let open Event in
+  array_elt_begin trace;
   Printf.fprintf trace.file
     "{\"name\": \"%s\", \"cat\": \"PERF\", \"ph\":\"%s\", \"ts\":%Ld, \"pid\": \
-     %d, \"tid\": %d%t},\n"
+     %d, \"tid\": %d%t}\n"
     evt.name ph (ts_to_us evt.ts) evt.ring_id evt.ring_id
     (match args with
     | None -> ignore
