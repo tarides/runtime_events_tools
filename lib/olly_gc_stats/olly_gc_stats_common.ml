2936584e49c36cea9303a3097f3bb40a1285682b
Edwin Török <edwin@tarides.com>
2026-09-18 09:32:26 +0100

    olly validate-gc-stats: compat with OCaml <5.4
    And address review comments about labeling lo,hi

--- lib/olly_gc_stats/olly_gc_stats_common.ml
+++ lib/olly_gc_stats/olly_gc_stats_common.ml
@@ -197,7 +197,7 @@ let check_eq field pp ~expected actual =
     Error [ Format.asprintf "%s: %a <> %a" field pp actual pp expected ]
   else Ok ()
 
-let check_range field pp lo hi actual =
+let check_range field pp ~lo ~hi actual =
   if actual < lo || actual > hi then
     Error [ Format.asprintf "%s: %a ∉ [%a, %a]" field pp actual pp lo pp hi ]
   else Ok ()
@@ -210,30 +210,34 @@ let validate_domain_stat (t : Json.Gc_stats.t)
     Error [ Printf.sprintf "Domain stat map key not an integer: %s" key ]
   else
     Ok ()
-    &&& check_range "0 <= gc_time <= wall_time" pp_s 0. ds.wall_time ds.gc_time
-    &&& check_range "0 <= gc_time <= global.gc_time" pp_s 0. t.gc_time
+    &&& check_range "0 <= gc_time <= wall_time" pp_s ~lo:0. ~hi:ds.wall_time
           ds.gc_time
-    &&& check_range "0 <= wall_time <= global.wall_time" pp_s 0. t.wall_time
-          ds.wall_time
-    &&& check_range "0 <= gc_overhead <= 100" pp_s 0. 100. t.gc_overhead
+    &&& check_range "0 <= gc_time <= global.gc_time" pp_s ~lo:0. ~hi:t.gc_time
+          ds.gc_time
+    &&& check_range "0 <= wall_time <= global.wall_time" pp_s ~lo:0.
+          ~hi:t.wall_time ds.wall_time
+    &&& check_range "0 <= gc_overhead <= 100" pp_s ~lo:0. ~hi:100. t.gc_overhead
 
 let validate_distr_latency (t : Json.Gc_stats.t) (key, (latency : float)) =
   if key |> int_of_string_opt |> Option.is_none then
     Error [ Printf.sprintf "Latency map key not an integer: %s" key ]
   else
     Ok ()
-    &&& check_range "min_latency <= latency <= max_latency" pp_s t.min_latency
-          t.max_latency latency
+    &&& check_range "min_latency <= latency <= max_latency" pp_s
+          ~lo:t.min_latency ~hi:t.max_latency latency
 
 let validate_outliers (t : Json.Gc_stats.t) (outliers : Json.Gc_stats.outliers)
     =
-  check_range "0 <= outliers" Format.pp_print_int 0 Int.max_int outliers.count
-  &&& check_range "0 <= mean_latency <= max_latency" Format.pp_print_float 0.
-        outliers.max_latency outliers.mean_latency
+  check_range "0 <= outliers" Format.pp_print_int ~lo:0 ~hi:Int.max_int
+    outliers.count
+  &&& check_range "0 <= mean_latency <= max_latency" Format.pp_print_float
+        ~lo:0. ~hi:outliers.max_latency outliers.mean_latency
   &&& check_range "t.min_latency <= mean_latency <= t.max_latency"
-        Format.pp_print_float t.min_latency t.max_latency outliers.mean_latency
+        Format.pp_print_float ~lo:t.min_latency ~hi:t.max_latency
+        outliers.mean_latency
   &&& check_range "t.min_latency <= max_latency <= t.max_latency"
-        Format.pp_print_float t.min_latency t.max_latency outliers.max_latency
+        Format.pp_print_float ~lo:t.min_latency ~hi:t.max_latency
+        outliers.max_latency
 
 let validate_domain_alloc_stat (t : Json.Gc_stats.t)
     (key, (da : Json.Gc_stats.domain_alloc_stat)) =
@@ -244,19 +248,21 @@ let validate_domain_alloc_stat (t : Json.Gc_stats.t)
     &&& check_eq "total = minor - promoted + major" Format.pp_print_int
           ~expected:(da.minor - da.promoted + da.major)
           da.total
-    &&& check_range "0 <= promoted_pct <= 100" Format.pp_print_float 0. 100.
-          da.promoted_pct
-    &&& check_range "total <= t.total_heap" Format.pp_print_int 0
-          (Float.to_int t.allocations.total_heap)
+    &&& check_range "0 <= promoted_pct <= 100" Format.pp_print_float ~lo:0.
+          ~hi:100. da.promoted_pct
+    &&& check_range "total <= t.total_heap" Format.pp_print_int ~lo:0
+          ~hi:(Float.to_int t.allocations.total_heap)
           da.total
-    &&& check_range "minor <= t.minor_heap" Format.pp_print_int 0
-          (Float.to_int t.allocations.minor_heap)
+    &&& check_range "minor <= t.minor_heap" Format.pp_print_int ~lo:0
+          ~hi:(Float.to_int t.allocations.minor_heap)
           da.minor
-    &&& check_range "promoted <= t.promoted_words" Format.pp_print_int 0
-          (Float.to_int t.allocations.promoted_words)
+    &&& check_range "promoted <= t.promoted_words" Format.pp_print_int ~lo:0
+          ~hi:(Float.to_int t.allocations.promoted_words)
           da.promoted
-    &&& check_range "major <= t.major_heap" Format.pp_print_int 0
-          (t.allocations.major_heap |> Option.value ~default:0. |> Float.to_int)
+    &&& check_range "major <= t.major_heap" Format.pp_print_int ~lo:0
+          ~hi:
+            (t.allocations.major_heap |> Option.value ~default:0.
+           |> Float.to_int)
           da.major
 
 let validate_assoc_map t f map =
@@ -273,23 +279,27 @@ let validate_domain_alloc_stats t opt =
   | None, false -> Ok ()
   | Some stats, _ -> validate_assoc_map t validate_domain_alloc_stat stats
 
+let current_version = 2
+
 let validate_json (t : Json.Gc_stats.t) =
   let domains = List.length t.domain_stats in
-  check_range "1 <= version <= 2" Format.pp_print_int 1 2 t.version
-  &&& check_range "0 <= cpu_time <= wall_time*domains" pp_s 0.
-        (t.wall_time *. float_of_int domains)
+  check_range "1 <= version <= 2" Format.pp_print_int ~lo:1 ~hi:current_version
+    t.version
+  &&& check_range "0 <= cpu_time <= wall_time*domains" pp_s ~lo:0.
+        ~hi:(t.wall_time *. float_of_int domains)
         t.cpu_time
-  &&& check_range "0 <= gc_time <= cpu_time" pp_s 0. t.cpu_time t.gc_time
-  &&& check_range "0 <= gc_overhead <= 100" Format.pp_print_float 0. 100.
-        t.gc_overhead
-  (* x86-46 and RISC-V 5-level paging: 57 bits,
+  &&& check_range "0 <= gc_time <= cpu_time" pp_s ~lo:0. ~hi:t.cpu_time
+        t.gc_time
+  &&& check_range "0 <= gc_overhead <= 100" Format.pp_print_float ~lo:0.
+        ~hi:100. t.gc_overhead
+  (* x86-64 and RISC-V 5-level paging: 57 bits,
       anything larger is likely a bug *)
-  &&& check_range "1 <= max_rss_kb" Format.pp_print_int 0
-        ((1 lsl 57) - 1)
+  &&& check_range "1 <= max_rss_kb" Format.pp_print_int ~lo:0
+        ~hi:((1 lsl 57) - 1)
         t.max_rss_kb
   &&& validate_assoc_map t validate_domain_stat t.domain_stats
   &&& check_range "min_latency <= mean_latency <= max_latency"
-        Format.pp_print_float t.min_latency t.max_latency t.mean_latency
+        Format.pp_print_float ~lo:t.min_latency ~hi:t.max_latency t.mean_latency
   &&& validate_outliers t t.outliers
   &&& validate_domain_alloc_stats t t.domain_alloc_stats
   &&& check_eq "total_heap = minor - promoted + major" Format.pp_print_int
@@ -298,20 +308,20 @@ let validate_json (t : Json.Gc_stats.t) =
            +. Option.value ~default:0. t.allocations.major_heap
           |> Float.to_int)
         (Float.to_int t.allocations.total_heap)
-  &&& check_range "0 <= promoted_words <= minor_heap" Format.pp_print_float 0.
-        t.allocations.minor_heap t.allocations.promoted_words
-  &&& check_range "0 <= promoted_pct <= 100" Format.pp_print_float 0. 100.
-        t.allocations.promoted_pct
-  &&& check_range "0 <= collections.minor" Format.pp_print_int 0 Int.max_int
-        t.collections.minor
-  &&& check_range "0 <= collections.major" Format.pp_print_int 0 Int.max_int
-        t.collections.major
-  &&& check_range "0 <= collections.forced_major" Format.pp_print_int 0
-        Int.max_int t.collections.forced_major
-  &&& check_range "0 <= collections.compactions" Format.pp_print_int 0
-        Int.max_int t.collections.compactions
+  &&& check_range "0 <= promoted_words <= minor_heap" Format.pp_print_float
+        ~lo:0. ~hi:t.allocations.minor_heap t.allocations.promoted_words
+  &&& check_range "0 <= promoted_pct <= 100" Format.pp_print_float ~lo:0.
+        ~hi:100. t.allocations.promoted_pct
+  &&& check_range "0 <= collections.minor" Format.pp_print_int ~lo:0
+        ~hi:Int.max_int t.collections.minor
+  &&& check_range "0 <= collections.major" Format.pp_print_int ~lo:0
+        ~hi:Int.max_int t.collections.major
+  &&& check_range "0 <= collections.forced_major" Format.pp_print_int ~lo:0
+        ~hi:Int.max_int t.collections.forced_major
+  &&& check_range "0 <= collections.compactions" Format.pp_print_int ~lo:0
+        ~hi:Int.max_int t.collections.compactions
 
-let current_version = 2
+let singleton x = [ x ]
 
 let validate_gc_stats jsonlines files =
   let pp_list =
@@ -328,7 +338,7 @@ let validate_gc_stats jsonlines files =
                let res =
                  line
                  |> Jsont_bytesrw.decode_string ~file Json.Gc_stats.jsont
-                 |> Result.map_error List.singleton
+                 |> Result.map_error singleton
                in
                let res = Result.bind res validate_json in
                let res =
@@ -337,7 +347,7 @@ let validate_gc_stats jsonlines files =
                      line
                      |> Jsont_bytesrw.decode_string ~file
                           Json.Gc_stats.version_only_jsont
-                     |> Result.map_error List.singleton
+                     |> Result.map_error singleton
                    in
                    match ver with
                    | Error _ as e -> e
@@ -357,7 +367,7 @@ let validate_gc_stats jsonlines files =
            let reader = Bytesrw.Bytes.Reader.of_in_channel ch in
            let res =
              Jsont_bytesrw.decode ~locs:true ~file Json.Gc_stats.jsont reader
-             |> Result.map_error List.singleton
+             |> Result.map_error singleton
            in
            Result.bind res validate_json
        in
